938d8551e840c3618ba45a379d01517f2ddccf2c
judygab <ivditigabeskiria@gmail.com>
2021-06-18 16:31:26 -0400

    added email provider for auth

--- timeboxing-app/pages/api/auth/[...nextauth].js
+++ timeboxing-app/pages/api/auth/[...nextauth].js
@@ -1,7 +1,25 @@
 import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
 
 const options = {
-  site: process.env.NEXTAUTH_URL
+  site: process.env.NEXTAUTH_URL,
+  providers: [
+  Providers.Email({
+      server: {
+        port: 465,
+        host: 'smtp.gmail.com',
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
 }
 
 export default (req, res) => NextAuth(req, res, options)
