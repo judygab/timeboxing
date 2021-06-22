c9aa6b6c446a4386c1428186bfa5ac0031590714
judygab <ivditigabeskiria@gmail.com>
2021-06-21 21:02:26 -0400

    added navbar

--- timeboxing-app/pages/_app.js
+++ timeboxing-app/pages/_app.js
@@ -1,10 +1,13 @@
 import '../styles/globals.css'
 import { Provider } from 'next-auth/client'
+import Layout from '../components/Layout';
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
+    <Layout>
+      <Provider session={pageProps.session}>
+        <Component {...pageProps} />
+      </Provider>
+    </Layout>
   )
 }
